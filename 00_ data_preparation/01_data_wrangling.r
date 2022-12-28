f86fd89f261a6f3673ea22829ca6d436333538f1
Amanda Rodrigues <120829907+amandarnds@users.noreply.github.com>
2022-12-27 23:23:43 -0800

    ✨ feat: data_wrangling
    Add new code

--- 00_ data_preparation/01_data_wrangling.r
+++ 00_ data_preparation/01_data_wrangling.r
@@ -15,3 +15,23 @@ colnames(ds)<- c("CheckingAcctStat","Duration", "CreditHistory","Purpose",
                  "SexAndStatus", "OtherDetorsGuarantors","PresentResidenceTime", 
                  "Property","Age","OtherInstallments","Housing","ExistingCreditsAtBank", 
                  "Job","NumberDependents","Telephone","ForeignWorker", "CreditStatus")
+
+#05 - general information 
+dim(ds)
+str(ds)
+
+#06 - Distribution of target variable
+
+#absolute frequency
+fa<- table(ds$CreditStatus)
+
+#relative frequency
+fr<- round((prop.table(fa))*100)
+fr
+#barplot
+graph_1 <- barplot(fr, xlab="Credit Status (Target Variable)",
+        ylab="Absolute Frequency",col=c("seagreen", "yellowgreen"),
+        main="Frequency(%)",ylim=c(0, lim*1.15))
+text(x=c(graph_1), y=fr, labels=fr, pos=3)
+lim <- par()$usr[4]
+box()
