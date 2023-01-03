2a13bc3e68d5edce6ca0488beba386722334d55b
Amanda Rodrigues <120829907+amandarnds@users.noreply.github.com>
2023-01-02 20:16:15 -0800

    ✨ feat: data_wrangling
    Adding new code about exploratory analyses

---
+++ 00_ data_preparation/02_data_exploration.R
@@ -0,0 +1,25 @@
+
+#It will be done a basic data exploration 
+
+#01 - General Information 
+#View dataset 
+View(ds1)
+
+#dimension
+dim(ds1)
+
+#types of variables 
+str(ds1)
+#As show above, we have two types of variables, categorical and numerical
+
+#Fiding unique values for each columns 
+# It's a important parameter to understand which column is categorical and which one is numerical
+# Typically if the number of unique values are < 20 then the variable is likely to be a category otherwise numerical
+uv<- apply(ds1,2,function(x)length(unique(x)))
+uv
+
+
+#02 - Missing Values 
+colSums(is.na(ds1))
+#No missing values in this data.Therefore,won´t be necessary to  treat them.
+
