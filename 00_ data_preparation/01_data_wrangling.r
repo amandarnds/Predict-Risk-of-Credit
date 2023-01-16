a99b58ada61c4d8fe400d61e3a47130db97660c0
Amanda Rodrigues <120829907+amandarnds@users.noreply.github.com>
2023-01-16 07:39:29 -0800

    💥 fix: data_wrangling
    Correction of  data wrangling(remove target analyses and balance of dataset), the analyse of target variable is from data analyses and balance of data set should be  used in training data

--- 00_ data_preparation/01_data_wrangling.r
+++ 00_ data_preparation/01_data_wrangling.r
@@ -6,9 +6,6 @@ getwd()
 ds<- data.frame(read.csv("credit.csv",header =TRUE, stringsAsFactors = TRUE))
 View(ds)
 
-#03 - checking dimension
-dim(ds)
-
 #04 - name the columns 
 colnames(ds)<- c("CheckingAcctStat","Duration", "CreditHistory","Purpose",
                  "CreditAmount","SavingsBonds","Employment", "InstallmentRatePecnt",
@@ -16,46 +13,20 @@ colnames(ds)<- c("CheckingAcctStat","Duration", "CreditHistory","Purpose",
                  "Property","Age","OtherInstallments","Housing","ExistingCreditsAtBank", 
                  "Job","NumberDependents","Telephone","ForeignWorker", "CreditStatus")
 
-#05 - general information 
+#05 - general information
+#dimension
 dim(ds)
+#types of variables 
 str(ds)
 
-#06 - Distribution of target variable
+#05 - Missing Values 
+colSums(is.na(ds))
+#No missing values in this data.Therefore,will not be necessary to  treat them.
 
-#absolute frequency
-fa<- table(ds$CreditStatus)
+#06- Duplicate values 
+sum(duplicated(ds))
+#No duplicated values.So, no action 
 
-#relative frequency
-fr<- round((prop.table(fa))*100)
-fr
-#barplot
-graph_1 <- barplot(fr, xlab="Credit Status (Target Variable)",
-        ylab="Relative Frequency",col=c("seagreen", "yellowgreen"),
-        main="Frequency(%)",ylim=c(0, lim*1.15))
-text(x=c(graph_1), y=fr, labels=fr, pos=3)
-lim <- par()$usr[4]
-box()
-#When we performing Classification,we have to make sure that 
-#there is a balance in the the distribution of each class,
-#otherwise it impacts the Machine Learning algorithms ability to learn all the classe.
-#As shown at the graph graph_1 _Frequency of target variable,
-#we have the class are unbalanced. Therefore, it will be necessary fix it.
-
-#07 - Balancing the dataset 
-
-# To balance the datset, it'll be used the  oversampling technique with
-# the ROSE package,where the minority class ("2") will be increased
-library(ROSE)
-ds1 <- ROSE(CreditStatus ~ ., data = ds, seed = 1)$data
-dim(ds1)
-View(ds1)
-
-fr1<- round(prop.table(table(ds1$CreditStatus))*100)
-fr1
-#Graph
-graph_2 <- barplot(fr1, main="Frequency (%)",
-                   col=c("darkblue", "lightblue"),
-                   xlab="Credit Status (Target Variable)",
-                   ylab="Relative Frequency",ylim = c(0, 90))
-text(fr1, x=c(graph_2), y=fr, labels=fr, pos=3)
-#As showed in the graph_2,, we have a balanced dataset now
+#07- Grouped values 
+View(ds)
+#According with the dataset, we don't have grouped values in the variables. 
